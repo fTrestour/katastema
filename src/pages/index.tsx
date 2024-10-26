fb7e555fad62b2ff40b4b561b6be7dcb63a8f0f8
Fabien <ftrestour@gmail.com>
2024-10-26 14:15:49 +0200

    🔧Biome config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import Layout from "src/layout";
 export default function Home() {
   return (
     <Layout title="Welcome to My Website" lang="en">
-      <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 class="text-4xl font-bold">Welcome to My Website</h1>
+      <div class="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <h1 class="font-bold text-4xl">Welcome to My Website</h1>
       </div>
     </Layout>
   );

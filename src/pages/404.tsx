f562509f37df7850e5522945ae528608afa127a9
Fabien <ftrestour@gmail.com>
2024-11-01 22:08:40 +0100

    🚚 Rename colors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,18 +4,18 @@ import Layout from "src/layout";
 export default function NotFound() {
   return (
     <Layout title="Katastema">
-      <div class="flex min-h-screen flex-col items-center justify-center gap-5 bg-steel-grey font-mono">
-        <h1 class="font-bold text-5xl text-violet">404</h1>
+      <div class="flex min-h-screen flex-col items-center justify-center gap-5 bg-dark font-mono">
+        <h1 class="font-bold text-5xl text-pink">404</h1>
 
-        <p class="z-10 max-w-xl text-center text-gold text-lg">
+        <p class="z-10 max-w-xl text-center text-lg text-yellow">
           Relax, you're lost.
         </p>
 
         <a
           href="/"
-          class="group relative mt-5 w-fit border-4 border-gold border-r-0 p-2 pr-3 before:absolute before:top-0 before:right-0 before:h-full before:w-1 before:bg-gold before:transition-all before:duration-200 before:content-['_'] hover:before:w-full focus:outline-none"
+          class="group relative mt-5 w-fit border-4 border-yellow border-r-0 p-2 pr-3 before:absolute before:top-0 before:right-0 before:h-full before:w-1 before:bg-yellow before:transition-all before:duration-200 before:content-['_'] hover:before:w-full focus:outline-none"
         >
-          <span class="relative font-semibold text-gold before:transition-all before:duration-200 group-hover:text-steel-grey">
+          <span class="relative font-semibold text-yellow before:transition-all before:duration-200 group-hover:text-dark">
             Go back home
           </span>
         </a>

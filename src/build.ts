e57e4ee86912a86411458f81baf63b909c004518
Fabien <ftrestour@gmail.com>
2024-11-01 22:09:15 +0100

    ♻️ Move build functions to utils

--- src/build.ts
+++ src/build.ts
@@ -1,35 +1,10 @@
-import { promises as fs } from "node:fs";
-import { join } from "node:path";
-import { Glob } from "bun";
-import { getEnv } from "./utils";
+import { buildPages, copyStaticFiles, createSiteMap, getEnv } from "./utils";
 
-async function renderPages() {
+async function render() {
   const { siteUrl } = getEnv();
-
-  const glob = new Glob("**/*.tsx");
-
-  const builtFilenames = new Array<string>();
-  const pages = "src/pages";
-  for await (const path of glob.scan(pages)) {
-    const file = join(pages, path);
-
-    const { default: Page } = await import(file);
-    const html = Page();
-
-    const builtFilename = path.replace(".tsx", ".html");
-
-    builtFilenames.push(builtFilename);
-
-    await fs.mkdir(join("dist", builtFilename, ".."), { recursive: true });
-    await fs.writeFile(join("dist", builtFilename), html, "utf8");
-  }
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${builtFilenames.map((filename) => `<url><loc>${siteUrl}${filename}</loc></url>`).join("\n  ")}
-</urlset>`;
-
-  await fs.writeFile(join("dist", "sitemap.xml"), sitemap, "utf8");
+  const builtFilenames = await buildPages();
+  await copyStaticFiles();
+  await createSiteMap(builtFilenames, siteUrl);
 }
 
-renderPages().catch(console.error);
+render().catch(console.error);
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import { Glob } from "bun";
+import { promises as fs } from "node:fs";
+import { join } from "node:path";
+
 export function getEnv() {
   const siteUrl = process.env["SITE_URL"];
 
@@ -14,4 +18,42 @@ export function getEnv() {
     siteUrl,
     siteLang,
   };
+}
+
+export async function createSiteMap(builtFilenames: string[], siteUrl: string) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${builtFilenames.map((filename) => `<url><loc>${siteUrl}${filename}</loc></url>`).join("\n  ")}
+</urlset>`;
+
+  await fs.writeFile(join("dist", "sitemap.xml"), sitemap, "utf8");
+}
+
+export async function buildPages() {
+  const glob = new Glob("**/*.tsx");
+  const pages = "src/pages";
+
+  const builtFilenames = new Array<string>();
+  for await (const path of glob.scan("src/pages")) {
+    const file = join(pages, path);
+
+    const { default: Page } = await import(file);
+    const html = Page();
+
+    const builtFilename = path.replace(".tsx", ".html");
+
+    builtFilenames.push(builtFilename);
+
+    await fs.mkdir(join("dist", builtFilename, ".."), { recursive: true });
+    await fs.writeFile(join("dist", builtFilename), html, "utf8");
+  }
+  return builtFilenames;
+}
+
+export async function copyStaticFiles() {
+  const glob = new Glob("**/*");
+  for await (const path of glob.scan("src/static")) {
+    await fs.mkdir(join("dist", path, ".."), { recursive: true });
+    await fs.copyFile(join("src/static", path), join("dist", path));
+  }
 }
